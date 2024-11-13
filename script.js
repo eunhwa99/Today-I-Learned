877bf68212e694b17b458c8a1b995668c6aa4903
eunhwa99 <ekdusp89@gmail.com>
2024-11-13 22:37:27 +0900

    Add JS file and add feature for hiding the form

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+  } else {
+    form.classList.add("hidden");
+  }
+});
