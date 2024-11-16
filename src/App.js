3f0de9dfdf0e5db82fb671875ad0c9029734543e
eunhwa99 <ekdusp89@gmail.com>
2024-11-17 00:14:36 +0900

    Add component and render it to App component - CategoryFilter component 생성 후, <CategoryFilter /> 로 렌더링

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import "./style.css";
 // App component ->  앞 글자가 대문자 (naming convention)
 function App() {
   return (
-    // html: class -> jsx: className
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
-        <h1>Today I Learned</h1>
-      </div>
+    <div>
+      {/* html: class -> jsx: className */}
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            height="68"
+            width="68"
+            alt="Today I Learned Logo"
+          />
+          <h1>Today I Learned</h1>
+        </div>
 
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+      <CategoryFilter />
+    </div>
   );
+}
+
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
 }
 export default App;
