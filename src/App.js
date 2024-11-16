3907a3209ca725802a0f4011c3cf43b3788fe273
eunhwa99 <ekdusp89@gmail.com>
2024-11-17 00:07:12 +0900

    Add jsx to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+// App component ->  앞 글자가 대문자 (naming convention)
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    // html: class -> jsx: className
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
   );
 }
-
 export default App;
